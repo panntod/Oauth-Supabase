bf6df6b15932c546226c9e2a6318f91344813cbd
panntod <pandhu.munjalindra@gmail.com>
2024-05-12 08:43:28 +0700

    refactor: Initialize dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Toaster } from "sonner";
 
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Toaster richColors position="top-center" />
     <App />
   </React.StrictMode>
 );
