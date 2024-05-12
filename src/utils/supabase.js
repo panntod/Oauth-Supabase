cbcca10088c7656c5661fe9298db2e1c1df0e70b
panntod <pandhu.munjalindra@gmail.com>
2024-05-12 08:44:24 +0700

    feat: Create function create client supabase

---
+++ src/utils/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  import.meta.env.VITE_API_URL,
+  import.meta.env.VITE_API_KEY
+);
+
+export default supabase;
